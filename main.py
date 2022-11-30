d191980bbd9e0f74b2c2793ba84749b66f2055a3
Bart <madejbart>
2022-11-30 23:05:55 +0200

    new functions

--- main.py
+++ main.py
@@ -441,8 +441,8 @@ thaiFood.open_restaurant()
 """
 class User:
     def __init__(self, first_name, last_name):
-        self.user_info['name'] = first_name
-        self.user_info['lastname'] = last_name
+        self.first_name = first_name
+        self.last_name = last_name
     def build_user(self, **user_info):
 
 
